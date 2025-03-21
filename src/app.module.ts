8476f5767d57e1626d0545c9b993c3b319dbd6e8
carlomonterrey <carlosmonterrey17@gmail.com>
2025-03-21 12:43:50 -0400

    Configurate the moongodb cloud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+
+    MongooseModule.forRoot('mongodb+srv://carlosmonterrey:Karlos8200@bloodbank.au9pv.mongodb.net/?retryWrites=true&w=majority&appName=Bloodbank', {
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
